cadec4efcec351c17b97f6c27aaf8c790553fe58
alohne <aaron.lohner@mail.mcgill.ca>
2021-06-23 17:13:38 -0400

    tweak bash script

--- sniffer.sh
+++ sniffer.sh
@@ -4,6 +4,6 @@ cd protos
 protoc --cpp_out=../proto_gen --python_out=../proto_gen sniffed_info.proto
 cd ..
 mv proto_gen/sniffed_info.pb.h include/
-g++ -o sniffer sniffer.cpp server.cpp proto_gen/sniffed_info.pb.cc -I/mnt/c/Users/Aaron/COMP_Research/DyMonD/include -lpcap -lboost_filesystem -lboost_system `pkg-config --cflags --libs protobuf`
+g++ -o sniffer sniffer.cpp server.cpp proto_gen/sniffed_info.pb.cc -Iinclude -lpcap -lboost_filesystem -lboost_system `pkg-config --cflags --libs protobuf`
 # Start up sniffer
 ./sniffer
