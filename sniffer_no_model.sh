23c2015bdc2d3213b19028bbebdc5acfce2a0e9c
alohne <aaron.lohner@mail.mcgill.ca>
2021-09-24 11:54:45 -0400

    rename files

--- sniffer_no_model.sh
+++ sniffer_no_model.sh
@@ -3,7 +3,7 @@ cd protos
 # Compile protos for Python controller script
 protoc --python_out=../proto_gen sniffed_info.proto
 cd ..
-g++ -o sniffer src/sniffer_no_model-va-encoding-bugs.cpp src/server.cpp -Iinclude -lpcap -lboost_filesystem -lboost_system -pthread
+g++ -o sniffer src/sniffer_no_model.cpp src/server.cpp -Iinclude -lpcap -lboost_filesystem -lboost_system -pthread
 mkdir -p captures flows json logs
 # Start up sniffer
 case $1 in
